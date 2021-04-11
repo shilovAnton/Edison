2609e6a647ebe2b8b3d4055aeac617f5988b3d52
shilov <shilov.a.a.1986@gmail.com>
2021-04-11 21:53:33 +0700

    Финал1

--- page.php
+++ page.php
@@ -15,36 +15,37 @@
 <body>
 
 <div class="h-100 bg-light.bg-gradient text-dark">
-
-    <?php
-    foreach ($psychic as $value) : ?>
-        <div class="col-0 p-3">
-            <div class="card mx-auto border border-primary" style="width: 18rem;">
-                <div class="card-header">
-                    <h4 class="card-title">Экстрасенс <?= $value->id; ?></h4>
-                </div>
-                <img src="<?= $value->get_img(); ?>" class="card-img-top" alt="Изображение аватара экстрасенса">
-                <div class="card-body">
-                    <h6 class="card-title text-center">Уровень достоверности</h6>
-                    <h2 class="card-title text-center"><?= $value->level; ?></h2>
-                    <?php
-                    if ($value->get_history_answer()) : ?>
-                        <h6 class="card-subtitle mb-2 text-muted text-center">История догадок :</h6>
-                        <p class="card-body text-muted">
-                            <?php
-                            foreach ($value->get_history_answer() as $v) : ?>
-                                <?= $v; ?>;
-                            <?php
-                            endforeach; ?>
-                        </p>
-                    <?php
-                    endif; ?>
+    <div class="row row-cols-1 row-cols-lg-3 g-20 g-lg-20 mt-3 my-3 g-0">
+        <?php
+        foreach ($psychic as $value) : ?>
+            <div class="col">
+                <div class="card mx-auto border border-primary mt-3 my-3" style="width: 18rem;">
+                    <div class="card-header">
+                        <h4 class="card-title">Экстрасенс <?= $value->id; ?></h4>
+                    </div>
+                    <img src="<?= $value->get_img(); ?>" class="card-img-top" alt="Изображение аватара экстрасенса">
+                    <div class="card-body">
+                        <h6 class="card-title text-center">Уровень достоверности</h6>
+                        <h2 class="card-title text-center"><?= $value->level; ?></h2>
+                        <?php
+                        if ($value->get_history_answer()) : ?>
+                            <h6 class="card-subtitle mb-2 text-muted text-center">История догадок :</h6>
+                            <p class="card-body text-muted">
+                                <?php
+                                foreach ($value->get_history_answer() as $v) : ?>
+                                    <?= $v; ?>;
+                                <?php
+                                endforeach; ?>
+                            </p>
+                        <?php
+                        endif; ?>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php
-    endforeach; ?>
-
+        <?php
+        endforeach; ?>
+    </div>
+</div>
     <div class="alert alert-dark">
         <p>История загаданных чисел :</p>
         <?php
